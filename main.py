81e78f554e2d6bb05825433b739713b5722fe7e4
DMeenaRK <sganeshraj72_bai28@mepcoeng.ac.in>
2026-02-24 11:18:30 +0530

    pattern print in python

---
+++ main.py
@@ -0,0 +1,15 @@
+def pattern(row,col):
+    for i in range(row):
+        for j in range(col):
+            if(i%2==0 ):
+                if( j%2==0):
+                    print(" + ",end="")
+                else:
+                    print("--",end="")
+            else:
+                if(j%2==0):
+                    print(" | ",end="")
+                else:
+                    print(". ",end="")
+        print()            
+pattern(7,7)
