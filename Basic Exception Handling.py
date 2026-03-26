efa44d87f6fece0e2590374773f0073d4755aec1
DMeenaRK <sganeshraj72_bai28@mepcoeng.ac.in>
2026-03-26 12:17:38 +0530

    Basic Exception Handling

---
+++ Basic Exception Handling.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    t = int(input())
+    
+    for _ in range(t):
+        try:
+            a, b = input().split()
+            print(int(a) // int(b))
+        except (ZeroDivisionError, ValueError) as e:
+            print("Error Code:", e)
