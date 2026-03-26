1f79d3b5910aad0a661bc31784c6fd861f87b362
DMeenaRK <sganeshraj72_bai28@mepcoeng.ac.in>
2026-03-26 12:18:00 +0530

    Captain's room

---
+++ Captain's room.py
@@ -0,0 +1,6 @@
+k = int(input())
+room_numbers = list(map(int, input().split()))
+unique_rooms = set(room_numbers)
+captain_room = (sum(unique_rooms) * k - sum(room_numbers)) // (k - 1)
+
+print(captain_room)
