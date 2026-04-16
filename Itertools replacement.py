28ac9c34c2c175a9bd7c626bbc2edde1ad14ca74
DMeenaRK <sganeshraj72_bai28@mepcoeng.ac.in>
2026-04-16 23:03:14 +0530

    Itertools replacement

---
+++ Itertools replacement.py
@@ -0,0 +1,8 @@
+from itertools import combinations_with_replacement
+s, k = input().split()
+s_sorted = sorted(s)
+k = int(k)
+
+combinations = combinations_with_replacement(s_sorted, k)
+for combo in combinations:
+    print("".join(combo))
